0236b5aca607754fd7e2b64716eb5901ae24e921
Krittin Chaowakarn <krittin.chao@gmail.com>
2024-07-30 18:03:53 +0700

    Create d-ary-heap.cpp

---
+++ 2-Sorting-and-Order-Statistics/6-Heapsort/Problems/d-ary-heap.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DAryHeap {
+    private :
+        vector<int> heap;
+        int dAry;   // If dAry is 2, then it is a binary heap.
+
+        int heapDepth() {
+            return log_a_to_base_b(heap.size() - 1, dAry) + 1;
+        }
+
+
+
+        int parent(int i) {
+            return (i - 1) / dAry;
+        }
+
+        int firstChild(int i) {
+            return dAry * i + 1;
+        }
+
+        int lastChild(int i) {
+            return dAry * (i + 1);
+        }
+
+        void maxHeapify(int i, int heap_size) {
+            int fChild = firstChild(i);
+            int lChild = lastChild(i);
+            
+            while (fChild < heap_size && fChild <= lChild) {
+                if (heap[i] < heap[fChild]) {
+                    swap(heap[i], heap[fChild]);
+                    maxHeapify(fChild, heap_size);
+                }
+                fChild++;
+            }
+        }
+
+
+        int log_a_to_base_b(int a, int b) {
+            return ceil(log2(a) / log2(b));
+        }
+    
+    public : 
+
+        void buildMaxHeap() {
+            int heap_size = heap.size();
+            for (int i = parent(heap.size()); i >= 0; i--) {
+                maxHeapify(i, heap_size);
+            }
+        }
+
+        DAryHeap(const vector<int> arr, const int d_Ary) : heap(arr), dAry(d_Ary) {
+            buildMaxHeap();
+        }
+
+        int maxHeapMaximum() {
+            if (heap.size() < 1) {
+                throw runtime_error("Heap Underflow");
+            }
+
+            return heap[0];
+        }
+
+        int maxHeapExtractionMax() {
+            int heap_size = heap.size();
+            int max = maxHeapMaximum();
+            heap[0] = heap[heap_size - 1];
+            heap.pop_back();
+            maxHeapify(0, heap_size);
+
+            return max;
+        }
+
+        void maxHeapIncreaseKey(int index, int key) {
+            if (index >= heap.size()) {
+                throw runtime_error("Index is out of bounds");
+            }
+            if (key < heap[index]) {
+                throw runtime_error("New Key is Smaller than Current Key");
+            }
+
+            heap[index] = key;
+
+            while(index > 0 &&  heap[parent(index)] < heap[index]) {
+                swap(heap[parent(index)], heap[index]);
+                index = parent(index);
+            }
+        }
+
+        void maxHeapInsert(int key) {
+            heap.push_back(INT_MIN);
+            maxHeapIncreaseKey(heap.size() - 1, key);
+        }
+
+        int searchMaxHeap(int key) {
+            for(int i = 0; i < heap.size(); i++) {
+                if (heap[i] == key) {
+                    return i;
+                }
+            }
+            throw runtime_error("The Heap Does Not Contain the Key");
+        }
+
+        void maxHeapDelete(int key) {
+            int index = searchMaxHeap(key);
+            swap(heap[index], heap[heap.size() - 1]);
+            heap.pop_back();
+            
+            maxHeapify(index, heap.size());
+        }
+
+        void constructSortedArray() {
+            int heap_size = heap.size();
+
+            for (int i = heap.size() - 1; i > 0; i--) {
+                swap(heap[0], heap[i]);
+                heap_size--;
+                maxHeapify(0, heap_size);
+            } 
+        }
+
+        void printHeap() {
+            int i = 0;
+            for (int depth = 0; depth < heapDepth(); depth++) {
+                int num = pow(dAry, depth);
+                while (num != 0 && i < heap.size()) {
+                    cout << heap[i] << " ";
+                    if ((num + 2)%3 == 0) {
+                        // cout << "  " << num << "  "; 
+                        cout << " | ";
+                    }
+                    i++;
+                    num--;
+                }
+                cout << endl;
+            }
+            cout << endl;
+        }
+        
+        void displayArray() {
+            for (int i = 0; i < heap.size(); ++i)
+                cout << heap[i] << " ";
+            cout << "\n\n";
+        }
+};
+
+int main() {
+    // vector<int> vec = {9, 16, 3, 2, 4, 8, 5, 14, 12, 17, 42, 25, 6};
+    vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+
+    DAryHeap heap1(vec, 3);
+
+    heap1.printHeap();
+
+    int maxi = heap1.maxHeapExtractionMax();
+    cout << "MAX : " << maxi << endl;
+    heap1.printHeap();
+
+    heap1.constructSortedArray();
+
+    heap1.displayArray();
+
+    heap1.buildMaxHeap();
+
+    heap1.displayArray();
+
+    return 0;
+}
