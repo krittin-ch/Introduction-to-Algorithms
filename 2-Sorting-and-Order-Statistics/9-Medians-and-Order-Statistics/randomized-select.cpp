0d7433c266f43a80cd4ff0508034b27323138033
Krittin Chaowakarn <krittin.chao@gmail.com>
2024-08-10 10:31:36 +0700

    Update randomized-select.cpp

--- 2-Sorting-and-Order-Statistics/9-Medians-and-Order-Statistics/randomized-select.cpp
+++ 2-Sorting-and-Order-Statistics/9-Medians-and-Order-Statistics/randomized-select.cpp
@@ -2,43 +2,6 @@
 
 using namespace std;
 
-int randomizedPartition(vector<int> &vec, int p, int r) {
-    int index = rand() % (vec.size() - 1);
-    int pivot = vec[index];
-    // cout << "PIVOT : " << pivot << endl;
-
-    swap(vec[index], vec[r]);
-
-    int i = p - 1;
-    int j = p;
-
-    while (j < r) {
-        if (vec[j] < pivot) {
-            swap(vec[j], vec[++i]);
-        }
-        j++;
-    }
-    swap(vec[++i], vec[r]);
-
-    return j;
-}
-
-int randomizedSelect(vector<int> vec, int p, int r, int i) {
-    if (p == r) {
-        return vec[p];
-    }
-    int q = randomizedPartition(vec, p, r);
-    int k = q - p + 1;
-
-    if (i == k) {
-        return vec[q];
-    } else if (i < k) {
-        return randomizedSelect(vec, p, q, i);
-    } else {
-        return randomizedSelect(vec, q + 1, r, i - k);
-    }
-}
-
 void displayArray(vector<int> vec) {
     for (int num : vec) {
         cout << num << " ";
@@ -46,17 +9,63 @@ void displayArray(vector<int> vec) {
     cout << endl;
 }
 
+int partition(vector<int> &vec, int p, int r) {
+    int pivot = vec[r];
+    int i = p - 1;
+    int j = p;
+
+    while (j < r) {
+        if (vec[j] < vec[r]) {
+            swap(vec[++i], vec[j]);
+        }
+        j++;
+    }
+    
+    swap(vec[++i], vec[r]);
+
+    return i;
+}
+int randomizedPartition(vector<int> &vec, int p, int r) {
+    int length = p - r + 1;
+
+    if (length > 1) {
+        int index = rand() % length;
+        swap(vec[index], vec[r]);
+    }
+    // cout << "PIVOT : " << pivot << endl;
+
+    return partition(vec, p, r);
+}
+
+int randomizedSelect(vector<int> &vec, int p, int r, int i) {
+    if (p == r) {
+        return vec[p];
+    }
+
+    int q = randomizedPartition(vec, p, r);
+    displayArray(vec);
+    int k = q - p + 1;
+
+    cout << "K = " << k << endl;
+
+    if (i == k) {
+        return vec[q];
+    } else if (i < k) {
+        return randomizedSelect(vec, p, q - 1, i);
+    } else {
+        return randomizedSelect(vec, q + 1, r, i - k);
+    }
+}
+
 int main() {
     srand(time(0));
-    vector<int> vec = {1, 4, 2, 5, 7, 9, 3, 6};
+    vector<int> vec = {4, 3, 2, 6, 1, 5, 10, 8, 7, 9};
 
-    randomizedPartition(vec, 0, vec.size() - 1);
+    int order = 6;
 
-    displayArray(vec);
+    int num = randomizedSelect(vec, 0, vec.size() - 1, order);
 
-    int min = randomizedSelect(vec, 0, vec.size() - 1, 6);
-
-    cout << "MINIMUM : " << min << endl;
+    cout << order << "-th order : " << num << endl;
 
     return 0;
 }
